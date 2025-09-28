7239a1bb695da66c41f3596c8ed2dfe380cf5ad6
electr-2 <i2512022@gl.cc.uec.ac.jp>
2025-09-28 19:13:06 +0900

    feat: first commit とりあえずデフォルトのやつのリンクと色だけ変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#363c70",
+        accent: "#f5dc3d",
+        "accent-dark": "#000d8a",
+        black: "rgb(15, 18, 25)",
+        gray: "rgb(96, 115, 159)",
+        "gray-light": "rgb(229, 233, 240)",
+        "gray-dark": "rgb(34, 41, 57)",
+      },
+      backgroundImage: {
+        "gray-gradient": "linear-gradient(rgba(229, 233, 240, 0.5), #fff)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
