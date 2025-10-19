40fa6fec0d846beb9cbf2b24e07c56359917530c
electr-2 <i2512022@gl.cc.uec.ac.jp>
2025-10-20 00:30:55 +0900

    refactor: いらない画像を消し、記事のフォールバック画像を変更した

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "でんじのブログ";
-export const SITE_DESCRIPTION = "でんじのブログへようこそ！";
+export const SITE_TITLE = "電２ you do";
+export const SITE_DESCRIPTION =
+	"電２ you doは、電２が技術や日常や強い思想について発信するブログです。";
