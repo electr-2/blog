02f3a00b2e42a5ffaad845fd298449483c9905d6
electr-2 <i2512022@gl.cc.uec.ac.jp>
2025-10-20 01:45:37 +0900

    feat: markdownが改行を許すようにした

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import remarkBreaks from "remark-breaks";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -10,4 +11,7 @@ export default defineConfig({
   site: "https://example.com",
   base: "/",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkBreaks],
+  },
 });
